6db4d6ad285129fcf10a7c997b121b91b569355d
javaknows <juanpserranor@gmail.com>
2021-11-03 05:46:17 -0400

    Update ERC677.sol

--- contracts/v0.4/token/ERC677.sol
+++ contracts/v0.4/token/ERC677.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.8;
 
-import "./linkERC20.sol";
+import "https://github.com/javaknows/LinkToken/blob/master/contracts/v0.4/token/linkERC20.sol";
 
 contract ERC677 is linkERC20 {
   function transferAndCall(address to, uint value, bytes data) returns (bool success);
